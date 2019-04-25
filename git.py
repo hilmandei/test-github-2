7b89396b4fc427bcf0156ebf0749ce4ed808df40
hilmandei <hilman.revisionery@gmail.com>
2019-04-25 11:25:10 +0700

    ini commit ke-3

--- git.py
+++ git.py
@@ -4,11 +4,15 @@ print(' hallo guyss')
 # git init ==> to start git (nanti ada file hidden 'git
 # git status ==> untuk cek status
 
-# untuk langkah
+# untuk langkah save dengan git
 # git add . === > untuk semua file
 # git add (filename) ===> jika mau file tertentu
 # git commit -m ('nama lable') ====> untuk memberi label
+
+# langkah untuk melihat versi sebelumnya
 # git log ===> untuk melihat log
+# git log --oneline ===> untuk melihat commit
+# git checkout (idgit) ===> untuk kembali ke commit berapa
 
 # =========================================================================
 # commit kedua sekarang
