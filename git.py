43819569951fa79f1e808e0f26880fc7cbac20b1
hilmandei <hilman.revisionery@gmail.com>
2019-04-26 09:42:55 +0700

    ini commint ke 5

--- git.py
+++ git.py
@@ -4,18 +4,20 @@ print(' hallo guyss')
 # git init ==> to start git (nanti ada file hidden 'git
 # git status ==> untuk cek status
 
-# untuk langkah save dengan git
+# ++++++ untuk langkah save dengan git ++++++++
 # git add . === > untuk semua file
 # git add (filename) ===> jika mau file tertentu
 # git commit -m ('nama lable') ====> untuk memberi label
 
-# langkah untuk melihat versi sebelumnya
+# ++++++ langkah untuk melihat versi sebelumnya +++++++
 # git log ===> untuk melihat log
 # git log --oneline ===> untuk melihat commit
 # git checkout (commitid) ===> untuk kembali ke commit berapa
 # git checkout master ====> untuk kembali ke versi terakhir
 
-
+# ===========================================================================
+#
+# ++++++langkah untuk upload ke web github +++++++
 # git push origin (master) =====> untuk upload ke github (master) file terkahir yang kita edit
 
 # =========================================================================
@@ -23,6 +25,19 @@ print(' hallo guyss')
 # git reset (commitid) --hard ====> untuk mereset file
 
 # git clone (link) ======> untuk mengcopy
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # commit keempat sekarang
